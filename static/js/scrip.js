12cb2834156a1404546aedc3cc3163e56f8455fc
bolar756 <bolarinwamartins79@gmail.com>
2024-04-29 16:38:53 +0100

    added payment flow

--- static/js/scrip.js
+++ static/js/scrip.js
@@ -5,3 +5,9 @@ faqItems.forEach(faqItem => {
     faqItem.classList.toggle('active');
   });
 });
+function myFunction() {
+    document.querySelector('.parents1').toggle('none')
+    document.querySelector('.parents2').toggle('none')
+}
+myFunction()
+setInterval(myFunction, 3000);
