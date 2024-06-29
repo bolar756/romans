38c732ad36323abd6ea328d863a92a19a3382ec1
bolar756 <bolarinwamartins79@gmail.com>
2024-06-29 12:05:23 +0100

    removed staticfiles_build

--- staticfiles_build/static/js/rate.js
+++
@@ -1,6 +0,0 @@
-const form = document.querySelector('form')
-form.addEventListener('submit',function(event){
-    event.preventDefault()
-    console.log('mean')
-    window.open('/' , as='_self')
-})
